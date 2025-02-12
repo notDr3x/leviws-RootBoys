5691a522f5654f3e94ca479e8ab7c88de22f0557
MintaDuku <84637071+MintaDuku@users.noreply.github.com>
2025-02-12 08:29:34 +0100

    Modifica al database

--- RootBoysProject/leviws-RootBoys-main/leviws-2/leviws2.sql
+++ RootBoysProject/leviws-RootBoys-main/leviws-2/leviws2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 10, 2025 alle 17:36
+-- Creato il: Feb 12, 2025 alle 08:27
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
